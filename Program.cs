0114d07a4c03e3e36963bed48805eca7536d6524
tutneshta <tutneshta@yandex.ru>
2022-09-01 13:09:05 +0300

    start

--- Program.cs
+++ Program.cs
@@ -6,7 +6,109 @@ namespace Results_7_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Client client = new Client();
+            
+            
+            Console.ReadKey();
         }
+
+
+    }
+
+    abstract class Delivery
+    {
+        private string Address;
+
+        public string address
+        {
+            get
+            {
+                return Address;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    Address = value;
+                }
+                else Console.WriteLine("введите адрес");
+
+            }
+        }
+
+
+    }
+     class Client
+    {
+        private string Name;
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    Name = value;
+                }
+                else Console.WriteLine("введите имя");
+            }
+        }
+
+        private string telephone;
+        public string Telephone 
+        {
+            get
+            {
+                return Telephone;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    Telephone = value;
+                }
+                else Console.WriteLine("введите телефон");
+            }
+        }
+
+
+
+    }
+
+    class HomeDelivery : Delivery
+    {
+
+
+
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
     }
 }
