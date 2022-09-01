5559799e7610fc0db5a00f9490c46a9b3c5a15fb
tutneshta <tutneshta@yandex.ru>
2022-09-01 13:01:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Results_7_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
